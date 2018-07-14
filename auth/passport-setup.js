11c342fd522814534c65da64b7e3505298fb933a
Lee Warrick <leewarrick@gmail.com>
2018-07-13 22:11:29 -0400

    implement google Oauth2, passport, sequelize

---
+++ auth/passport-setup.js
@@ -0,0 +1,48 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const db = require('../models');
+
+passport.use( 
+    new GoogleStrategy(
+        {
+            //options
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: '/auth/google/redirect'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            //console.log(profile);
+            
+            db.User.findOrCreate(
+                {
+                    where: {
+                        id: profile.id
+                    },
+                    defaults: {
+                        email: profile.emails[0].value
+                    }
+                }
+            ).spread((user, created) => {
+                //console.log(user.dataValues);
+
+                done(null, user);
+            });
+            
+           
+        }
+    )
+);
+
+passport.serializeUser((user, done) => {
+    // console.log("serialize: " + JSON.stringify(user))
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    db.User.findById(id).then((user) => {
+        // console.log("deserialize:" + JSON.stringify(user));
+        done(null, user);
+    });
+});
+
+module.exports = passport;
--- server.js
+++ server.js
@@ -1,18 +1,42 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cookieSession = require('cookie-session');
+const passport = require('./auth/passport-setup');
 const PORT = process.env.PORT || 3001;
+const db = require('./models');
+const authRouter = require('./routes/auth-routes');
+const apiRouter = require('./routes/api-routes');
 const app = express();
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// set up session cookies
+app.use(cookieSession({
+  name: process.env.COOKIE_NAME,
+  maxAge: 1000 * 60 * 60 * 24,
+  keys: [
+    process.env.COOKIE_KEY1,
+    process.env.COOKIE_KEY2,
+    process.env.COOKIE_KEY3,
+  ]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+app.use('/api', apiRouter);
+app.use('/auth', authRouter);
 
 // Send every other request to the React app
 // Define any API routes before this runs
@@ -20,6 +44,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
-});
+db.sequelize.sync({})
+  .then(
+    app.listen(PORT, () => {
+      console.log(`🌎 ==> Server now on port ${PORT}!`);
+    })
+  );
